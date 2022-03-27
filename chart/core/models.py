834536a19195a28bfb4a6a40ea08564962b4b5bf
hwandev12 <husan.ahmedov777@yandex.com>
2022-03-27 08:03:55 +0500

    Second edit

--- chart/core/models.py
+++ chart/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+	class Meta:
+		verbose_name = "Post"
+		verbose_name_plural = "My posts"
+
+	name = models.CharField(max_length=50)
+	valus = models.TextField()
+
+	def __str__(self):
+		return self.name
