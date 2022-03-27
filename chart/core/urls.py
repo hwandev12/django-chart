1d3405a393db889a7361c28298eb819babee6567
hwandev12 <husan.ahmedov777@yandex.com>
2022-03-27 10:03:05 +0500

    Fourth edit

--- chart/core/urls.py
+++ chart/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, pieMarket
 
 urlpatterns = [
     path('', home),
+    path('market/', pieMarket)
 ]
--- chart/core/views.py
+++ chart/core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'blog/index.html')
+
+def pieMarket(request):
+	return render(request, 'blog/market-pie.html')
