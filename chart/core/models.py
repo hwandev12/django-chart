dd87f834ccfc907ec8050b6f2788b75918fb720a
hwandev12 <husan.ahmedov777@yandex.com>
2022-03-27 20:40:51 +0500

    sixth edit

--- chart/core/models.py
+++ chart/core/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
 		verbose_name_plural = "My posts"
 
 	name = models.CharField(max_length=50)
-	valus = models.TextField()
+	year = models.IntegerField()
 
 	def __str__(self):
 		return self.name
--- chart/core/views.py
+++ chart/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
-	return render(request, 'blog/index.html')
+	posts = Post.objects.all()
+	context = {
+		"posts": posts
+	}
+	return render(request, 'blog/index.html', context)
 
+# Piemarket
 def pieMarket(request):
 	return render(request, 'blog/market-pie.html')
